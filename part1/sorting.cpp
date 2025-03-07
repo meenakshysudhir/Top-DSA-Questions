4b91aa6894c09228fbafe47ce6c5e2c302d2af89
Meenakshy Sudhir <ukms2004@gmail.com>
2025-03-07 11:55:46 +0530

    insertion sort

--- part1/sorting.cpp
+++ part1/sorting.cpp
@@ -13,6 +13,22 @@ void printArray(int arr[],int len){
     }
 
 }
+    void bubble_sort(int arr[], int n)
+{
+    for(int i = 0; i < n-1;i++)
+    {
+
+        for(int j = 0; j < n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+
+                swap(arr,j,j+1);
+            }
+        }
+    }
+    printArray(arr,n);
+}
 void selection_sort(int arr[],int len)
 {
     
@@ -29,9 +45,36 @@ void selection_sort(int arr[],int len)
     }
 printArray(arr ,len);
 }
+void insertion_sort(int arr[],int n){
+
+    for(int i = 1; i < n;i++){
+        int j = i-1;
+        int key = arr[i];
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    printArray(arr, n);
+}
 int main(void)
 {
     int arr[] = {13,46,24,52,20,9};
     int size = sizeof(arr)/sizeof(arr[0]);
-    selection_sort(arr,size);
+    
+    cout << "Enter 1 for selection sort\nEnter 2 for bubble sort\nEnter 3 for insertion sort\nEnter 4 for quick sort\nChoice : ";
+    int ch;
+    cin >> ch;
+    switch(ch){
+        case 1:
+            selection_sort(arr,size);
+            break;
+        case 2:
+            bubble_sort(arr,size);
+        case 3:
+            insertion_sort(arr,size);
+    }
+    
 }
