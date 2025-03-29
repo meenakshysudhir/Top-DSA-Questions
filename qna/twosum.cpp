92d27dbb207025f658d7d491943989192494fb7b
Meenakshy Sudhir <ukms2004@gmail.com>
2025-03-29 13:01:59 +0530

    solution two sum

---
+++ qna/twosum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map <int,int> m;
+    
+            for(int i = 0;i < nums.size();i++){
+                m[nums[i]] = i;
+            }
+            vector<int> solution;
+            int flag  = false;
+            for(int i = 0; i < nums.size();i++){
+                int diff = target - nums[i];
+                auto got = m.find(diff);
+                if(got != m.end() && i!= got->second ){
+                    solution.push_back(i);
+                    solution.push_back(got->second);
+                    return solution;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    };
+    
