06f15c4837e685836b0efeed864caef4ed1c678b
Meenakshy Sudhir <ukms2004@gmail.com>
2025-04-13 17:08:33 +0530

    top freq by O(nlogn)

--- qna/topFreq.cpp
+++ qna/topFreq.cpp
@@ -1,26 +1,34 @@
-//given an int array nums and an int k, find the k most frequent elements
-
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<int> nums = {1,2,2,3,3,3};
+int k = 2;
+vector<int> res;
+
+void sorting(){
+    unordered_map<int,int> count;
+    for(auto i: nums){
+        count[i]++;
+    }
+    vector<pair<int,int>> arr;
+    for(const auto& i: count){
+        arr.push_back({i.second,i.first});
+    }
+    sort(arr.rbegin(),arr.rend());
+
+    for(int i =0;i < k;i++){
+        res.push_back(arr[i].second);
+    }
+    // return res;
+    
+}
 int main(void){
-
-    vector<int> nums = {1,2,2,3,3,3};
-    int k = 2;
-    int len =  nums.size();
-    vector<int> res(len,0);
-    for(auto i:nums){
-        res[i]++;
+   
+    sorting();
+    for(auto i:res){
+        cout << i << endl;
     }
-
-    unordered_map<int ,int> freq;
-    for(int i = 0; i < len;i++){
-        freq[res[i]] = i;
-    }
-    for(const auto i: freq){
-        cout << i.first <<" " << i.second << endl; 
-    }
-
 }
