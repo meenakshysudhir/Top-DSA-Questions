6b8a4d113c238b9af3041516ae38d6fae7e04b06
Meenakshy Sudhir <ukms2004@gmail.com>
2025-04-16 13:45:08 +0530

    sudoku

--- qna/sudoku.cpp
+++ qna/sudoku.cpp
@@ -1,22 +1,54 @@
 #include <everything.h>
 using namespace std;
 
-bool check3by3(vector<vector<char>> board, int row, int col) {
-  unordered_set<char> set;
-  int count = 0;
-  for (int i = row; i < row + 3; i++) {
-    for (int j = col; j < col + 3; j++) {
+int check3by3(vector<vector<char>> board) {
+
+  unordered_map<int, unordered_set<int>> map;
+  unordered_map<int, int> count;
+  int cn = 0;
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
       if (board[i][j] != '.') {
-        set.insert(board[i][j]);
-        count++;
+        if (i < 3 && j < 3) {
+          count[0]++;
+          map[0].insert(board[i][j]);
+        } else if (i < 3 && j < 6) {
+          count[1]++;
+          map[1].insert(board[i][j]);
+
+        } else if (i < 3 && j < 9) {
+          count[2]++;
+          map[2].insert(board[i][j]);
+        } else if (i < 6 && j < 3) {
+          map[3].insert(board[i][j]);
+          count[3]++;
+        } else if (i < 6 && j < 6) {
+          map[4].insert(board[i][j]);
+          count[4]++;
+        } else if (i < 6 && j < 9) {
+          map[5].insert(board[i][j]);
+          count[5]++;
+        } else if (i < 9 && j < 3) {
+          map[6].insert(board[i][j]);
+          count[6]++;
+        } else if (i < 9 && j < 6) {
+          map[7].insert(board[i][j]);
+          count[7]++;
+        } else {
+          map[8].insert(board[i][j]);
+          count[8]++;
+        }
       }
     }
   }
-  if (count != set.size()) {
-    return false;
+  for (const auto &[i, elements] : map) {
+    if (count[i] != elements.size()) {
+      return 0;
+    }
   }
-  return true;
+  return 1;
 }
+
 int checkrows(vector<vector<char>> board) {
 
   unordered_map<int, unordered_set<char>> map;
@@ -31,9 +63,29 @@ int checkrows(vector<vector<char>> board) {
     }
   }
 
-  int i = 0;
   for (const auto &[row, elements] : map) {
     if (count[row] != elements.size()) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int checkcols(vector<vector<char>> board) {
+  unordered_map<int, unordered_set<char>> map;
+  unordered_map<int, int> count;
+  for (int i = 0; i < 9; i++) {
+    // first column
+    count[i] = 0;
+    for (int j = 0; j < 9; j++) {
+      if (board[j][i] != '.') {
+        map[i].insert(board[j][i]);
+        count[i]++;
+      }
+    }
+  }
+  for (const auto &[col, elements] : map) {
+    if (count[col] != elements.size()) {
       return 0;
     }
   }
@@ -52,20 +104,15 @@ int main(void) {
       {'.', '.', '.', '.', '8', '.', '.', '7', '9'},
 
   };
-  int row = 0, col = 0;
-  bool flag;
-  // flag = check3by3(board, row, col);
-  do {
-    flag = check3by3(board, row, col);
-    if (col != 6) {
-      col += 3;
-    } else {
-      row += 3;
-      col = 0;
-    }
-  } while (flag && row != 9);
+  int flag = check3by3(board);
   cout << flag << endl;
-
   int flagr = checkrows(board);
   cout << flagr << endl;
+  int flagc = checkcols(board);
+  cout << flagc << endl;
+  if (flag && flagr && flagc) {
+    cout << "Valid soduku" << endl;
+  } else {
+    cout << "Invalid soduku" << endl;
+  }
 }
